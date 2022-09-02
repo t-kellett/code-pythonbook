c4c9c70e0ff7f4c863eafc98a753d093aa980255
t-kellett <t.p.kellett@gmail.com>
2022-09-02 15:53:51 +0100

    adding option

--- src/domain/model.py
+++ src/domain/model.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
+from typing import Optional
+
 
 @dataclass(frozen=True)
 class OrderLine:
@@ -8,7 +10,7 @@ class OrderLine:
     qty: int
 
 class Batch:
-    def __init__(self, reference: str, sku: str, qty: int, eta: date) -> None:
+    def __init__(self, reference: str, sku: str, qty: int, eta: Optional[date]) -> None:
         self.reference = reference
         self.sku = sku
         self.available_quantity = qty
