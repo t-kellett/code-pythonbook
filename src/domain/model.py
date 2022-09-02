04979c91cdb23032d38afcd5a284cf397148e648
t-kellett <t.p.kellett@gmail.com>
2022-09-02 17:17:52 +0100

    adding deallocation and refactoring

--- src/domain/model.py
+++ src/domain/model.py
@@ -14,8 +14,25 @@ class Batch:
                  ) -> None:
         self.reference = reference
         self.sku = sku
-        self.available_quantity = qty
+        self.purchased_quantity = qty
         self.eta = eta
+        self._allocated_lines = set()
 
-    def allocate(self, line) -> int:
-        self.available_quantity -= line.qty
+    def can_allocate(self, line) -> int:
+        return self.available_quantity >= line.qty and self.sku == line.sku
+
+    def allocate(self, line):
+        if self.can_allocate(line):
+            self._allocated_lines.add(line)
+
+    def deallocate(self, line):
+        if line in self._allocated_lines:
+            self._allocated_lines.remove(line)
+
+    @property
+    def allocated_quantity(self):
+        return sum(line.qty for line in self._allocated_lines)
+
+    @property
+    def available_quantity(self):
+        return self.purchased_quantity - self.allocated_quantity
--- tests/test_batches.py
+++ tests/test_batches.py
@@ -2,10 +2,37 @@ from datetime import date
 from domain.model import OrderLine, Batch
 
 
-def test_allocating_a_batch_reduces_available_quantity():
-    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = OrderLine("order-ref", "SMALL-TABLE", 2)
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int):
+    return (
+        Batch('batch-001', sku, batch_qty, eta=date.today()),
+        OrderLine('order-123', sku, line_qty)
+    )
 
+def test_can_reduce_batch_quantity_by_orderline_quantity():
+    batch, line = make_batch_and_line('WOODCHUCK-CHUCK', 20, 2)
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+def test_can_allocate_if_available_greater_than_or_equal_to_required():
+    large_batch, small_line = make_batch_and_line('BIG-TABLE', 20, 2)
+
+    assert large_batch.can_allocate(small_line)
+
+
+def test_cannot_allocate_if_available_less_than_required():
+    small_batch, large_line = make_batch_and_line('BIG-TABLE', 2, 10)
+
+    assert small_batch.can_allocate(large_line) is False
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch('batch-002', 'YELLOW-LORRY', 2, eta=date.today())
+    line = OrderLine('order-2', 'RED-LORRY', 2)
+
+    assert batch.can_allocate(line) is False
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line('LOVELY-LAMP', 10, 5)
+
+    batch.deallocate(unallocated_line)
+
+    assert batch.available_quantity == 10
